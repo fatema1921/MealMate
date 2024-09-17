23b85230f284b118b09f2837526e28b6f84a49ba
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2024-09-17 22:38:38 +0200

    Import routers to the app.js

--- server/app.js
+++ server/app.js
@@ -28,9 +28,11 @@ const Meal = require('./models/meal');
 //Routers
 const ingredientRouter = require('./controllers/ingredient/ingredientRoutes');
 
-// Importing routes 
-const userRoutes = require('./routes/userRoutes'); // Import user routes
-const recipeRoutes = require('./routes/recipeRoutes');
+//Importing Routers
+const calendarRouter = require('controllers/calendar/calendarRoutes');
+const mealRouter = require('controllers/meal/mealRoutes');
+app.use('/api', calendarRouter);
+app.use('/api', mealRouter);
 
 // Create Express app
 var app = express();
@@ -47,6 +49,8 @@ app.use(cors());
 app.use('/api', ingredientRouter)
 
 app.use(bodyParser.json());
+
+app.use(bodyParser.json());  // Parse JSON requests
 
 // Import routes
 app.get('/api', function(req, res) {
