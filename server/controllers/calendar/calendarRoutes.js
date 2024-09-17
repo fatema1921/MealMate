21a4d5ff53f5abdcf95357033fb7fcc845139e42
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2024-09-17 22:23:26 +0200

    Add relationship between meal and calendar

--- server/controllers/calendar/calendarRoutes.js
+++ server/controllers/calendar/calendarRoutes.js
@@ -25,4 +25,7 @@ router.delete('/:id', calendarController.deleteCalendarById);
 // Route to add a meal to a calendar
 router.post('/:calendarId/meals/:mealId', calendarController.addMealToCalendar);
 
+// Route to add a meal to a calendar
+router.post('/calendars/:calendarId/meals/:mealId', calendarController.addMealToCalendar);
+
 module.exports = router;
