2d6053daa4a566c3503d3bc30e4b0488751cf545
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2024-09-17 23:00:18 +0200

    test pipeline

--- server/app.js
+++ server/app.js
@@ -36,8 +36,8 @@ const mealRouter = require('./controllers/meal/mealRoutes');
 //Importing Routers
 const calendarRouter = require('./controllers/calendar/calendarRoutes');
 const mealRouter = require('./controllers/meal/mealRoutes');
-app.use('/api', calendarRouter);
-app.use('/api', mealRouter);
+//app.use('/api', calendarRouter);
+//app.use('/api', mealRouter);
 
 // Create Express app
 var app = express();
