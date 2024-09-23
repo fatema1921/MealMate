f7430f5a5367a3c2e4c02cc5e4c4dac42c5d8750
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2024-09-23 17:38:57 +0200

    Add relationship endpoints to Recipes

--- server/controllers/recipe/recipeRoutes.js
+++ server/controllers/recipe/recipeRoutes.js
@@ -9,4 +9,16 @@ router.post('/recipes', recipeController.createRecipe); // Create a new recipe
 router.put('/recipes/:id', recipeController.updateRecipe); // Update a recipe by ID
 router.delete('/recipes/:id', recipeController.deleteRecipe); // Delete a recipe by ID
 
+// Add an ingredient to a recipe
+router.post('/recipes/:id/ingredients', recipeController.addIngredientToRecipe);
+
+// Get all ingredients of a recipe
+router.get('/recipes/:id/ingredients', recipeController.getIngredientsOfRecipe);
+
+// Get a specific ingredient of a recipe by ingredientId
+router.get('/recipes/:recipeId/ingredients/:ingredientId', recipeController.getIngredientById);
+
+// Delete a specific ingredient from a recipe by ingredientId
+router.delete('/recipes/:recipeId/ingredients/:ingredientId', recipeController.deleteIngredientById);
+
 module.exports = router;
