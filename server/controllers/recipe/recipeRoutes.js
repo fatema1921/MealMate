62edf16591b5578875b995646e70755d131942ee
Adrian Niklasson <gusnikada@student.gu.se>
2024-09-27 10:28:51 +0200

    change in filestructure

--- server/controllers/recipe/recipeRoutes.js
+++ server/controllers/recipe/recipeRoutes.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const recipeController = require('./recipeController'); // Import the recipe controller
-
+const recipeController = require('../controllers/recipeController'); // Import the recipe controller
+const ingredientController = require('../ingredient/ingredientController'); 
 // Define recipe routes
 router.get('/recipes', recipeController.getAllRecipes); // Get all recipes
 router.get('/recipes/:id', recipeController.getRecipe); // Get a single recipe by ID
 router.post('/recipes', recipeController.createRecipe); // Create a new recipe
 router.put('/recipes/:id', recipeController.updateRecipe); // Update a recipe by ID
 router.delete('/recipes/:id', recipeController.deleteRecipe); // Delete a recipe by ID
+router.get('/ingredients/:ingredient_id/recipes', recipeController.getRecipesByIngredient);// Retrieve recipes by using an ingridient 
+router.patch('/recipes/:id', recipeController.patchRecipe); //  Update some fields for a specific recipe
 
-// Add an ingredient to a recipe
-router.post('/recipes/:id/ingredients', recipeController.addIngredientToRecipe);
+// Fetch recipes from TheMealAPI
+router.get('/search/:recipe', recipeController.searchRecipe);
 
-// Get all ingredients of a recipe
-router.get('/recipes/:id/ingredients', recipeController.getIngredientsOfRecipe);
-
-// Get a specific ingredient of a recipe by ingredientId
 router.get('/recipes/:recipeId/ingredients/:ingredientId', recipeController.getIngredientById);
 
-// Delete a specific ingredient from a recipe by ingredientId
 router.delete('/recipes/:recipeId/ingredients/:ingredientId', recipeController.deleteIngredientById);
 
 module.exports = router;
