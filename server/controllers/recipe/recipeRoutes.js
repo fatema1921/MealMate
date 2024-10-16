b2092237dc5d5c42839b6a2296dd5fd31d8fbf07
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2024-10-16 11:23:14 +0200

    Fix ingredient model

--- server/controllers/recipe/recipeRoutes.js
+++ server/controllers/recipe/recipeRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const recipeController = require('./recipeController'); // Import the recipe controller
 
 // Fetch recipes from External API
-//router.get('/fetch-and-save', recipeController.fetchAndSaveRecipes);
+// router.get('/fetch-and-save', recipeController.fetchAndSaveRecipes);
 
 // Define recipe routes
 router.get('/', recipeController.getAllRecipes); // Get all recipes
